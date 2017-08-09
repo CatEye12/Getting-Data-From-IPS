13443fff761a00b419d0088c80601907f9714855
CatEye12 <dashulechka20@yandex.ru>
2017-08-09 16:58:31 +0300

    Getting info about object by ID.

--- ConsoleApplicationGetDataViaCOM/Program.cs
+++ ConsoleApplicationGetDataViaCOM/Program.cs
@@ -7,25 +7,21 @@ using Intermech.Interfaces;
 using Intermech.Interfaces.Projects;
 using System.Runtime.Remoting;
 using Intermech.Kernel;
+using Intermech.Kernel.Search;
+using System.Data;
 
 namespace ComApi
 {
     class Program
     {
-
+        public static IUserSession session;
+        
         static void Main(string[] args)
         { 
             Program obj = new Program();
-            IUserSession session = obj.Connect("kb82", "");
-
-          
-
-            Console.WriteLine(session.UserName + "\n" + session.IsAdmin.ToString());
-            Console.ReadLine();
-
-            IDBObject my_obj = (IDBObject)session;
-              
-            
+            session = obj.Connect("kb82", "");
+            Console.WriteLine(session.UserName);
+            obj.Method();
             Console.ReadLine();
         }
         public IUserSession Connect(string loginName, string password)
@@ -38,9 +34,30 @@ namespace ComApi
             // Получаем смещение для текущего часового пояса и вызываем у сессии функцию авторизации
             DateTime now = DateTime.Now;
             TimeSpan ts = now - now.ToUniversalTime();
-            
             session.Login(loginName, password, Environment.MachineName, ts, 0);
             return session;
+        }
+
+        public void Method()
+        {
+            IDBObject obj = session.GetObject(748547);
+            Console.WriteLine("ProjectID of obj = " + obj.ProjectID.ToString() + "\n" + obj.Caption + "\n" + obj.SiteID + "\n" + obj.SubjectAreas);
+           // Console.ReadLine();
+
+            //IDBObjectCollection objCollection = session.GetObjectCollection(1052); // выбрать детали
+            IDBObjectCollection objCollection = session.GetObjectCollection(-1); // выбрать все объекты
+            ConditionStructure[] conditions = new ConditionStructure[]
+            {
+                new ConditionStructure("Заголовок объекта", RelationalOperators.StartString, "Кольцо", LogicalOperators.NONE, 0, true)
+            };
+
+            ColumnDescriptor[] descriptor = new ColumnDescriptor[]
+            {
+                new ColumnDescriptor(ObligatoryObjectAttributes.F_OBJECT_ID, AttributeSourceTypes.Auto, ColumnContents.ID, ColumnNameMapping.Default, SortOrders.DESC, 0),
+                new ColumnDescriptor(ObligatoryObjectAttributes.CAPTION, AttributeSourceTypes.Auto, ColumnContents.Text, ColumnNameMapping.Default, SortOrders.ASC, 1),
+            };
+            //Console.WriteLine("ObjCollection:" + objCollection.Select();
+            Console.ReadLine();
         }
     }
 }
